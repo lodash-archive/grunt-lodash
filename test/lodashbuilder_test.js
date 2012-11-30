a96167cff522c8b6fc1af175f23e88bdb6ef4fa4
asciidisco <public@asciidisco.com>
2012-11-30 11:36:02 +0100

    Surpress stdout from showing up in the test output, added some basic tests

--- test/lodashbuilder_test.js
+++ test/lodashbuilder_test.js
@@ -1,4 +1,6 @@
 var grunt = require('grunt');
+var builder = require('../lib/builder');
+
 // Load local tasks.
 grunt.loadTasks('tasks');
 
@@ -7,5 +9,23 @@ exports['require'] = {
   'use strict';
     // setup here
     done();
+  },
+  testSimpleLodashCustomBuilderOutput: function(test) {
+    'use strict';
+    test.expect(2) ;
+    var options = {
+      config: {
+        include: ['each'],
+        debug: true
+      }
+    };
+
+    var build = builder.init(grunt).build;
+    build(options, function (transport) {
+      test.equal(transport.type, 'content', 'Transport type has been set correctly')
+      test.ok(transport.content.length >= 1, 'Transport content has been generated')
+      test.done();
+    });
   }
+
 };
